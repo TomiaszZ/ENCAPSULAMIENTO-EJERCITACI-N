a3f5e19d7a4fa9c5f206fcc5a99cab01d7cbc7db
TomiaszZ <tomascarrizo2018@gmail.com>
2026-04-16 11:37:49 -0300

    Update 9.py
    hola

--- 9.py
+++ 9.py
@@ -6,21 +6,43 @@ si el juego está en oferta (si el precio es menor a un valor dado).
 '''
 class juego():
     def __init__(self, nombre, precio, genero):
-        self.nombre = nombre
-        self.precio = precio
-        self.genero = genero
+        self._nombre = nombre
+        self._precio = precio
+        self._genero = genero
     # getters
     def getNombre(self):
-        return self.nombre
+        return self._nombre
     def getPrecio(self):
-        return self.precio
+        return self._precio
     def getGenero(self):
-        return self.genero
+        return self._genero
+    
+    def mostrarinfo(self):
+        print(f" {self.getGenero()} , ${ self.getPrecio()}, {self.getGenero()}")
     
     def Verifoferta(self, valorin):
         try:
-            valor = int(valorin)
+            valor_limite = float(valorin)
+            if self._precio < valor_limite:
+                return True
+            else:
+                return False
         except ValueError:
             print("Introduce un valor valido")
+            return False
 
 if __name__ == "__main__":
+    juego1 = juego("Cyberpunk 2077", 30, "RPG")
+    juego1.mostrarinfo()
+    limite = 40
+    if juego1.Verifoferta(limite):
+        print(f"Oferta El precio es menor a ${limite}")
+    else:
+        print(f"No esta en oferta (Supera los ${limite})")
+
+    juego2 = juego("Elden Ring", 60, "Souls-like")
+    juego2.mostrarinfo()
+    if juego2.Verifoferta(40):
+        print("Oferta detectada")
+    else:
+        print("Precio normal zzZzZzZzZzZZz")
