cf52c788d035bf41dac6d5b86046aabe652ae5ce
TomiaszZ <tomascarrizo2018@gmail.com>
2026-04-16 11:44:29 -0300

    Update 6.py
    getters y encap

--- 6.py
+++ 6.py
@@ -1,26 +1,31 @@
-'''
 #6
-Crea una clase llamada Rectángulo que tenga atributos base y altura. 
-Implementa métodos para calcular el área y el perímetro del rectángulo.
-'''
+# Crea una clase llamada Rectangulo que tenga atributos base y altura. 
+# Implementa metodos para calcular el area y el perimetro del rectangulo.
 
 class Rectangulo:
     def __init__(self, altura, anchura):
-        self.altura = altura
-        self.anchura = anchura
+        self._altura = altura
+        self._anchura = anchura
+    
+    # Getters
+    def getAltura(self):
+        return self._altura
+    
+    def getAnchura(self):
+        return self._anchura
     
     def CalcularArea(self):
-        return self.altura * self.anchura
+        return self._altura * self._anchura
         
     def CalcularPerimetro(self):
-        return 2 * (self.altura + self.anchura)
+        return 2 * (self._altura + self._anchura)
 
 if __name__ == "__main__":
 
     mi_rectangulo = Rectangulo(10, 5)
     
-    print(f"altura: {mi_rectangulo.altura}")
-    print(f"Anchura: {mi_rectangulo.anchura}")
+    print(f"altura: {mi_rectangulo.getAltura()}")
+    print(f"Anchura: {mi_rectangulo.getAnchura()}")
     
     area = mi_rectangulo.CalcularArea()
     print(f"area calculada: {area}")
