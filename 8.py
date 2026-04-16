381a0578540afd080a64947206cd319b40ff0870
TomiaszZ <tomascarrizo2018@gmail.com>
2026-04-16 11:41:14 -0300

    Update 8.py
    getters y encap

--- 8.py
+++ 8.py
@@ -1,44 +1,52 @@
-'''
 #8
-Crea una clase llamada Producto que tenga atributos nombre, precio y stock. 
-Implementa metodos para aumentar y disminuir el stock, asegurandote de que no se pueda tener un stock negativo.
-'''
+# Crea una clase llamada Producto que tenga atributos nombre, precio y stock. 
+# Implementa metodos para aumentar y disminuir el stock, asegurandote de que no se pueda tener un stock negativo.
+
 class producto:
     def __init__(self, Nombre, precio):
-        self.nombre = Nombre
-        self.precio = precio
-        self.stock = 0
+        self._nombre = Nombre
+        self._precio = precio
+        self._stock = 0
     
-    # Setters
+    def getNombre(self):
+        return self._nombre
+
+    def getPrecio(self):
+        return self._precio
+
+    def getStock(self):
+        return self._stock
+    
     def AddStock(self, units):
         try:
             unidades = int(units)
-            self.stock += unidades
-            print(f"cantidad de stock de {self.nombre}: {self.stock}")
+            self._stock += unidades
+            print(f"cantidad de stock de {self._nombre}: {self._stock}")
         except ValueError:
             print("Introduce un valor valido")
             
     def RemoveStock(self, units):
         try:
             unidades = int(units)
-            if unidades > self.stock:
-                print(f"No puedes retirar {unidades} unidades. Solo hay {self.stock} en stock.")
+            if unidades > self._stock:
+                print(f"No puedes retirar {unidades} unidades. Solo hay {self._stock} en stock.")
             else:
-                self.stock -= unidades
-                print(f"cantidad de stock de {self.nombre}: {self.stock}")
+                self._stock -= unidades
+                print(f"cantidad de stock de {self._nombre}: {self._stock}")
         except ValueError:
             print("Introduce un valor valido")
 
 if __name__ == "__main__":
     prod1 = producto("Monitor Gamer", 300)
-    print(f"Producto: {prod1.nombre} | Precio: ${prod1.precio}")
+    print(f"Producto: {prod1.getNombre()} | Precio: ${prod1.getPrecio()}")
     
     prod1.AddStock(50)      
     prod1.RemoveStock(20)  
     prod1.RemoveStock(40)
+
     prod2 = producto("Teclado Mecanico", 85)
-    print(f"Producto: {prod2.nombre} | Stock Inicial: {prod2.stock}")
+    print(f"Producto: {prod2.getNombre()} | Stock Inicial: {prod2.getStock()}")
 
     prod2.AddStock(10)
-    prod2.RemoveStock("hola")
-    print(f"Stock final de {prod2.nombre}: {prod2.stock}")
+    prod2.RemoveStock("hola") 
+    print(f"Stock final de {prod2.getNombre()}: {prod2.getStock()}")
